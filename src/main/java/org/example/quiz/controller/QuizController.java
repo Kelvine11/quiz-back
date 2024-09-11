e92536d995150f8ea51306aa9f7e726ae028811b
Kelvine-Palet <kelvine1.palet@gmail.com>
2024-09-11 16:24:15 +0200

    Fix new

--- src/main/java/org/example/quiz/controller/QuizController.java
+++ src/main/java/org/example/quiz/controller/QuizController.java
@@ -2,21 +2,25 @@ package org.example.quiz.controller;
 
 import org.example.quiz.entity.Question;
 import org.example.quiz.entity.Quiz;
+import org.example.quiz.service.QuestionService;
 import org.example.quiz.service.QuizService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/quiz")
 public class QuizController {
 
     private final QuizService quizService;
+    private final QuestionService questionService;
 
-    public QuizController(QuizService quizService) {
+    public QuizController(QuizService quizService, QuestionService questionService) {
         this.quizService = quizService;
+        this.questionService = questionService;
     }
 
     @GetMapping("/index")
@@ -24,8 +28,11 @@ public class QuizController {
         return ResponseEntity.ok(quizService.getAllQuiz());
     }
 
-    @PostMapping("/new")
+    @PostMapping(value = "/new", consumes = {"application/json"})
     public ResponseEntity<Quiz> newQuiz(@RequestBody Quiz quiz) {
+        System.out.println(quiz);
+        Quiz quizz = quizService.createQuiz(quiz);
+        questionService.createManyQuestions(quiz.getQuestions().toArray(new Question[0]), quizz);
         return ResponseEntity.ok(quizService.createQuiz(quiz));
     }
 
@@ -46,6 +53,11 @@ public class QuizController {
         return ResponseEntity.ok(quiz);
     }
 
+    @GetMapping("/connect")
+    public Map<String, String> connect() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Connected to Quizz API");
+        return response;
 
-
+    }
 }
--- src/main/java/org/example/quiz/service/QuestionService.java
+++ src/main/java/org/example/quiz/service/QuestionService.java
@@ -2,15 +2,25 @@ package org.example.quiz.service;
 
 import org.example.quiz.dao.QuestionRepository;
 import org.example.quiz.entity.Question;
+import org.example.quiz.entity.Quiz;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
     public QuestionService(QuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
+    }
+
+    public void createManyQuestions(Question[] questions, Quiz quiz) {
+        for (Question question : questions) {
+            question.setQuiz(quiz);
+            questionRepository.save(question);
+        }
     }
 
     public Question createQuestion(Question question) {
